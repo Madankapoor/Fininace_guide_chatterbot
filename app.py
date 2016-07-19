105b8437538a6a2670b06d3f617c03e4f62aea63
Madan Kapoor <madankapoor10@gmail.com>
2016-07-19 06:31:18 +0000

    Auto commit

--- app.py
+++ app.py
@@ -1,27 +1,8 @@
 from flask import Flask,render_template,request
-from chatterbot import ChatBot
-from chatterbot.training.trainers import ListTrainer
-
-chatterbot = ChatBot("Training Example")
-chatterbot.set_trainer(ListTrainer)
-
-chatterbot.train([
-    "Hi there!",
-    "Hello",
-])
-
-chatterbot.train([
-    "Greetings!",
-    "Hello",
-])
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-        sr=chatterbot.get_response(request.form["message_box"])
-    else:
-        sr=""
-    return render_template("index.html",botreply=sr)
+    return render_template("index.html",botreply="")
     
