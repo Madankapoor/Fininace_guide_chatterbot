30cd608f2f7b67ad4231674f1453abdce2646f24
Madan Kapoor <madankapoor10@gmail.com>
2016-07-20 12:49:37 +0000

    Auto commit

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask,render_template,request
+from chatterbot import ChatBot
+from chatterbot.training.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot("Ron Obvious")
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+
+# Train based on the english corpus
+chatbot.train("chatterbot.corpus.english")
+
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return render_template("index.html",botreply="")
-    
+    if request.method == 'POST':
+        sr=chatbot.get_response(request.form["message_box"])
+    else:
+        sr="Hi, How can I help in your queries related to Investments ?"
+        
+    return render_template("index.html",botreply=sr)
