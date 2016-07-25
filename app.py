60b04f9c269497f16fe895a0101037e8bdb38eef
Madan Kapoor <madankapoor10@gmail.com>
2016-07-25 07:31:09 +0000

    Auto commit

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask,render_template,request
-from statichatter import  StaticChatterBot
+import aiml
+import os
 
+kernel = aiml.Kernel()
 
 app=Flask(__name__)
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml")
+    kernel.saveBrain("bot_brain.brn")
 
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        sr= StaticChatterBot.chatterbot.get_response(request.form["message_box"])
+        sr= kernel.respond(request.form["message_box"])
     else:
         sr="Hi, How can I help in your queries related to Investments ?"
         
